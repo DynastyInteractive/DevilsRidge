bafecfb57ed674bad1169c87e27162d80c628125
jayjaythebose <52707025+jayjaythebose@users.noreply.github.com>
2023-03-29 15:35:37 +0100

    Added Inventory and Book GUI

---
+++ Assets/Scripts/Menu/InventoyUI.cs
@@ -0,0 +1,19 @@
+
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class InventoyUI : MonoBehaviour
+{
+    Inventory inventory;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Inventory.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
