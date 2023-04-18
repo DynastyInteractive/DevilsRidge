b948e923adb176e26621b1e8268b01e568025792
jayjaythebose <52707025+jayjaythebose@users.noreply.github.com>
2023-04-18 16:34:06 +0100

    Main Menu final assets

--- Assets/Inventory.cs
+++ Assets/Inventory.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    internal static void instance()
+    {
+        throw new NotImplementedException();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
