3cc60951b7f61420a11bdf2519ea3f1f55c30213
BenHoldway03 <benholdway03@gmail.com>
2023-04-25 16:29:04 +0100

    Player Tracking Fixed

--- Assets/Scripts/Enemy AI/EnemyAIController.cs
+++ Assets/Scripts/Enemy AI/EnemyAIController.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -8,36 +7,36 @@ public class EnemyAIController : MonoBehaviour
 {
     [SerializeField] NavMeshAgent _nav;
 
-    [Tooltip("Starting position of the enemy")][SerializeField] Vector3 _startingPos;
+    [Tooltip("Starting position of the enemy")] [SerializeField] Vector3 _startingPos;
 
     [Tooltip("The different states")][SerializeField] enum State
     {
         Idle,
         Wandering,
-        Targetting,
+        Targeting,
         Attacking
     }
-    [Tooltip("The current State of the enemy")][SerializeField] State _currentState;
+
+    [Tooltip("The current State of the enemy")] [SerializeField] State _currentState;
 
     [Space(10)]
     [Header("Wandering")]
-    [Tooltip("Range for a new position")][SerializeField] int _wanderRadius = 10;
-    [Tooltip("Distance that the enemy can wander from the starting position")][SerializeField] int _maxWanderRadius = 20;
-    [Tooltip("Current position of the enemy")][SerializeField] Vector3 _currentPos;
-    [Tooltip("Next position for the enemy")][SerializeField] Vector3 _walkPoint;
-    [Tooltip("A check to see if the enemy can get a new position")][SerializeField] bool _newPos;
+    [Tooltip("Range for a new position")] [SerializeField] int _wanderRadius = 10;
+    [Tooltip("Distance that the enemy can wander from the starting position")] [SerializeField] int _maxWanderRadius = 20;
+    [Tooltip("Next position for the enemy")] [SerializeField] Vector3 _walkPoint;
+    [Tooltip("A check to see if the enemy can get a new position")] [SerializeField] bool _canGetNewPos;
 
     [Space(10)]
     [Header("Rotation")]
-    [Tooltip("Speed of the rotation")][SerializeField] float _rotationSpeed;
-    [Tooltip("Rotational angle to look to the target")][SerializeField] Quaternion _lookRotation;
-    [Tooltip("Vector between current position and target")][SerializeField] Vector3 _direction;
+    [Tooltip("Speed of the rotation")] [SerializeField] float _rotationSpeed;
+    [Tooltip("Rotational angle to look to the target")] [SerializeField] Quaternion _lookRotation;
+    [Tooltip("Vector between current position and target")] [SerializeField] Vector3 _direction;
 
     [Space(10)]
     [Header("Player Targetting")]
-    [Tooltip("Gets the position of the player")][SerializeField] List<GameObject> _player;
-    [Tooltip("List of Player GameObjects")][SerializeField] List<Vector3> _playerTransforms;
-    [Tooltip("ID of the player being checked/targetted")][SerializeField] int _playerID = -1;
+    [Tooltip("Gets the position of the player")] [SerializeField] List<GameObject> _player;
+    [Tooltip("List of Player GameObjects")] [SerializeField] List<Vector3> _playerPositions;
+    [Tooltip("ID of the player being checked/targetted")] [SerializeField] int _playerIndex = -1;
 
     // Start is called before the first frame update
     void Awake()
@@ -45,7 +44,7 @@ public class EnemyAIController : MonoBehaviour
         _nav = GetComponent<NavMeshAgent>();
         _startingPos = transform.position;
         _walkPoint = new Vector3(transform.position.x, transform.position.y - 0.4f, transform.position.z);
-        _newPos = true;
+        _canGetNewPos = true;
         _currentState = State.Idle;
     }
 
@@ -53,11 +52,11 @@ public class EnemyAIController : MonoBehaviour
     void Update()
     {
         //empties the positions of the players to replace them
-        _playerTransforms.Clear();
+        _playerPositions.Clear();
 
         //gets all of the players currently in game
         var _players = FindObjectsOfType<PlayerMovement>();
-        foreach(PlayerMovement player in _players) 
+        foreach (PlayerMovement player in _players)
         {
             if (!_player.Contains(player.gameObject))
             {
@@ -65,38 +64,38 @@ public class EnemyAIController : MonoBehaviour
                 _player.Add(player.gameObject);
             }
 
-            Transform _position = player.gameObject.GetComponent<Transform>();
-            _playerTransforms.Add(new Vector3(_position.position.x, _position.position.y, _position.position.z));
+            Vector3 _position = player.transform.position;
+            _playerPositions.Add(new Vector3(_position.x, _position.y, _position.z));
         }
 
-        /*foreach(Vector3 player in _playerTransforms)
+        //gets result of whether player can be targeted, and sets the state according to this
+        if(_currentState == State.Attacking)
         {
-            Debug.Log(player);
-        }*/
-
-        if (CanTarget() || _currentState == State.Wandering || (Random.Range(0, 10) < 3 && _currentState == State.Idle))
-        {
-            _currentState = State.Wandering;
-
-            if (_newPos)
-            {
-                _newPos = false;
-                StartCoroutine(NewPosition(false));
-            }
+            AttackPlayer();
         }
         else if (CanTarget())
         {
-            _currentState = State.Targetting;
+            _currentState = State.Targeting;
+            MoveToPlayer();
+        }
+        
+        else if (Random.Range(0, 10) < 3)
+        {
+            _currentState = State.Wandering;
+
+            if (_canGetNewPos)
+            {
+                _canGetNewPos = false;
+                StartCoroutine(NewPosition(false));
+            }
         }
     }
 
-    void MoveTo()
+    void Move()
     {
-        _currentPos = new Vector3(transform.position.x, transform.position.y - 0.9f, transform.position.z);
+        float _distance = Vector3.Distance(transform.position, _walkPoint);
 
-        float _distance = Vector3.Distance(_currentPos, _walkPoint);
-
-        if (_distance > 0.5)
+        if (_distance > 0.5f)
         {
             if (_direction != Vector3.zero)
             {
@@ -121,7 +120,7 @@ public class EnemyAIController : MonoBehaviour
 
     //Sets the new position when wandering
     //quickChange determines whether or not the enemy has to wait before getting a new position
-    public IEnumerator NewPosition(bool _quickChange) 
+    public IEnumerator NewPosition(bool _quickChange)
     {
         if (!_quickChange)
         {
@@ -142,50 +141,57 @@ public class EnemyAIController : MonoBehaviour
         NavMesh.SamplePosition(_randomDirection, out _hit, _wanderRadius, 1);
         _walkPoint = _hit.position;
 
-        MoveTo();
-        _newPos = true;
+        Move();
+        _canGetNewPos = true;
     }
 
     public bool CanTarget()
     {
-        float _distance = 10000000000;
-        
+        float distance = -1;
+
         //runs through all the players and gets the closest one
-        foreach (Vector3 players in _playerTransforms)
+        foreach (Vector3 playerPos in _playerPositions)
         {
-            if(Vector3.Distance(_currentPos, players) < _distance)
+            if (Vector3.Distance(transform.position, playerPos) < distance || distance == -1)
             {
-                _distance = Vector3.Distance(_currentPos, players);
-                _playerID = _playerTransforms.IndexOf(players);
+                distance = Vector3.Distance(transform.position, playerPos);
+                _playerIndex = _playerPositions.IndexOf(playerPos);
             }
         }
 
-        if (_playerID != -1)
+        //check to see if the player can be seen and is within targetting range
+        if (_playerIndex != -1 && distance < 20)
         {
-            Vector3 _playerPos = new Vector3(_playerTransforms[_playerID].x, _playerTransforms[_playerID].y, _playerTransforms[_playerID].z);
-            RaycastHit _hit;
-            if (Physics.Raycast(_currentPos, _playerPos, out _hit))
-            {
-                if (_hit.transform.name == _player[_playerID].name && _distance < 20)
-                {
-                    Debug.Log(gameObject.name + ": Can target player");
-                    return true;
-                }
-            }
+            Debug.Log(gameObject.name + ": Can target player, " + distance + " from player");  
+            return true;
         }
         else
         {
-            Debug.Log("No Player ID Found");
+            Debug.Log(gameObject.name + ": Can't target player" + distance + " from player");
+            //Debug.Log("distance greater than 20");
             return false;
         }
-
-        Debug.Log(gameObject.name + ": Can't target player");
-        return false;
     }
 
     void MoveToPlayer()
     {
-        _walkPoint = _playerTransforms[_playerID];
-        MoveTo();
+        _walkPoint = _playerPositions[_playerIndex];
+        
+        _nav.destination = _walkPoint;
+
+        if (Vector3.Distance(transform.position, _walkPoint) < 3)
+        {
+            _nav.isStopped = true;
+            _currentState = State.Attacking;
+        }
+        else
+        {
+            _nav.isStopped = false;
+        }
+    }
+
+    void AttackPlayer()
+    {
+        Debug.Log(_player[_playerIndex].name + " is being attacked!");
     }
 }
