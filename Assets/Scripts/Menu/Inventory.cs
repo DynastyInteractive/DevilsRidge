4ee5fd111060cb10f638bb5d6290fc8440d3dfd9
jayjaythebose <52707025+jayjaythebose@users.noreply.github.com>
2023-04-28 16:40:30 +0100

    Fixed Era and Invoked the add method

--- Assets/Scripts/Menu/Inventory.cs
+++ Assets/Scripts/Menu/Inventory.cs
@@ -19,6 +19,7 @@ public class Inventory : MonoBehaviour
     public void Add (Item item)
     {
         items.Add(item);
+        OnItemAdded?.Invoke();
         Debug.LogWarning("More than one instance of Inventory found!");
     }
 
