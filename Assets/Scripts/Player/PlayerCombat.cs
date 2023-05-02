8d9f2c8b5a2e4a91623e299303bb1f8f72e2cca7
BenHoldway03 <benholdway03@gmail.com>
2023-05-02 10:33:39 +0100

    Added Tooltips to variables

--- Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -1,32 +1,32 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerCombat : MonoBehaviour
 {
     #region Attack Variables
-    [SerializeField] float _heavyAttackMinTime = 0.5f;
-    [SerializeField] float _heavyAttackMaxTime = 2f;
-    [SerializeField] bool _canAttack;
-    [SerializeField] float _damage = 1f;
+    [Space(10)]
+    [Header("Attack Variables")]
+    [Tooltip("Minimum time to make a heavy attack")][SerializeField] float _heavyAttackMinTime = 0.5f;
+    [Tooltip("Maximum time for an attack, which is when it will auto attack")][SerializeField] float _heavyAttackMaxTime = 2f;
+    [Tooltip("A check to see if the player can attack")][SerializeField] bool _canAttack;
+    [Tooltip("The damage amount")][SerializeField] float _damage = 1f;
 
     PlayerInput Input;
     float m_AttackHoldTimer;
     #endregion
 
     #region Combo Variables
-    int m_comboAttackNum;
-    bool m_comboCooldownActive;
-    float m_comboCooldown = 10f;
+    [Space(10)]
+    [Header("Combo Variables")]
+    [Tooltip("The number of attacks the player has done in the chain")] int m_comboAttackNum;
+    [Tooltip("A check to see if the combo is active")] bool m_isComboActive;
+    [Tooltip("The time for the player to attack again before combo fails")] float m_comboTimer = 10f;
     #endregion
 
     void Start()
     {
         _canAttack = true;
-        m_comboCooldownActive = false;
+        m_isComboActive = false;
         m_comboAttackNum = 0;
         Input = new PlayerInput();
         Input.Enable();
@@ -35,15 +35,15 @@ public class PlayerCombat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(m_comboAttackNum > 0 && m_comboCooldownActive)
+        if(m_comboAttackNum > 0 && m_isComboActive)
         {
-            m_comboCooldown -= Time.deltaTime;
+            m_comboTimer -= Time.deltaTime;
         }
 
-        if(m_comboCooldown <= 0 && m_comboCooldownActive)
+        if(m_comboTimer <= 0 && m_isComboActive)
         {
             Debug.Log("Combo Failed");
-            m_comboCooldownActive = false;
+            m_isComboActive = false;
             m_comboAttackNum = 0;
         }
 
@@ -61,7 +61,7 @@ public class PlayerCombat : MonoBehaviour
 
         if (Input.Player.Attack.WasReleasedThisFrame() || m_AttackHoldTimer >= _heavyAttackMaxTime + 0.5f)
         {
-            if(m_comboAttackNum > 0 && m_comboCooldownActive)
+            if(m_comboAttackNum > 0 && m_isComboActive)
             {
                 Debug.Log("Combo Attack Number:" + m_comboAttackNum);
                 _damage *= 1.2f + (m_comboAttackNum * 0.2f);
@@ -88,8 +88,8 @@ public class PlayerCombat : MonoBehaviour
 
             Debug.Log("The damage hit was " + _damage);
 
-            m_comboCooldownActive = true;
-            m_comboCooldown = 5f;
+            m_isComboActive = true;
+            m_comboTimer = 5f;
             m_comboAttackNum++;
             m_AttackHoldTimer = 0;
             _damage = 1f;
@@ -103,3 +103,5 @@ public class PlayerCombat : MonoBehaviour
         _canAttack = true;
     }
 }
+
+
