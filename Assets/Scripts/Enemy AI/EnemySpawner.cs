1eaa161fa519bae933a40002366330c1c8ca235a
JamieSchooling <99397650+JamieSchooling@users.noreply.github.com>
2023-05-17 18:51:17 +0100

    Fixed enemy spawning

--- Assets/Scripts/Enemy AI/EnemySpawner.cs
+++ Assets/Scripts/Enemy AI/EnemySpawner.cs
@@ -10,47 +10,34 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] int[] numbers;
     [SerializeField] float spawnArea;
 
-    List<GameObject> _enemies;
-
     public float CampRadius
     {
         get { return campRadius; }
     }
-
     public GameObject[] Enemies
     {
         get { return enemies; }
     }
-
     private void Start()
     {
         SphereCollider col = GetComponent<SphereCollider>();
-        if(col != null) col.radius = campRadius;
+        if (col != null) col.radius = campRadius;
         SpawnEnemy();
-    }
-
-    private void OnDestroy()
-    {
-        DespawnEnemies();
     }
 
     void SpawnEnemy()
     {
-        foreach(GameObject enemy in enemies)
+        foreach (GameObject enemy in enemies)
         {
-            for(int i = 0; i < numbers[System.Array.IndexOf(enemies, enemy)];)
+            for (int i = 0; i < numbers[System.Array.IndexOf(enemies, enemy)];)
             {
                 EnemyController _enemyController = enemy.GetComponent<EnemyController>();
-
                 if (_enemyController.Enemy.isBoss) spawnArea = campRadius / 2;
                 else spawnArea = campRadius;
-
                 Vector3 randomPos = new Vector3(Random.Range(transform.position.x - spawnArea, transform.position.x + spawnArea), transform.position.y, Random.Range(transform.position.z - spawnArea, transform.position.z + spawnArea));
-
                 if (Vector3.Distance(randomPos, transform.position) <= spawnArea)
                 {
                     GameObject enemyObj = Instantiate(enemy, randomPos, Quaternion.identity);
-                    _enemies.Add(enemyObj);
                     EnemyController _enemyCont = enemyObj.GetComponent<EnemyController>();
                     _enemyCont.NearestCamp = gameObject;
                     i++;
@@ -59,13 +46,6 @@ public class EnemySpawner : MonoBehaviour
         }
     }
 
-    void DespawnEnemies()
-    {
-        foreach (GameObject enemy in enemies)
-        {
-            Destroy(enemy);
-        }
-    }
 
     private void OnDrawGizmos()
     {
